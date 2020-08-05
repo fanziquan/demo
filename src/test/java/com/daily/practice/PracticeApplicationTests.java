b84fe84edf38e983369cd253e172adb3c91fae8a
fan_coco <634164598@qq.com>
2020-08-05 23:13:06 +0800

    提交

---
+++ src/test/java/com/daily/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
